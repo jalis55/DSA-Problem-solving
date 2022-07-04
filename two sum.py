7ad09c025c9c8c156235cf302d5d935744d05c3d
JALIS MAHAMUD TARIF <40600638+jalis55@users.noreply.github.com>
2022-07-04 18:39:32 +0600

    Update two sum.py
    fix errors

--- two sum.py
+++ two sum.py
@@ -1,11 +1,11 @@
 nums = [2,7,11,15]
-target = 9
+target = 18
 prevmap={}
 for i,n in enumerate(nums):
-    diff=target-i
+    diff=target-n
     if diff in prevmap:
         print(prevmap[diff],i)
-    prevmap[n]=i 
-    print(prevmap)
+    else:
+        prevmap[n]=i
 
 
